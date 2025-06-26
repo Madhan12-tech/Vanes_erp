3f1386613d4ec5c5dad2ec4420c69a86ddb4c626
Madhan12-tech <madhancg.18@gmail.com>
2025-06-26 10:05:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -276,5 +276,5 @@ def customer():
 # ---------- Start ----------
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, port=10000)
+    app.run(debug=True, host='0.0.0.0', port=10000)
     
